bdcb7b783afb334da3de63605110f816b11464f1
David <hopperman@gmail.com>
2016-05-26 00:10:03 -0700

    Add solution to integerBreak from leetcode

--- integerBreak.js
+++ integerBreak.js
@@ -1,4 +1,45 @@
-// Given a positive integer n, break it into the sum of at least two positive integers and maximize the product of those integers. Return the maximum product you can get.
+/**
+ * @param {number} n
+ * @return {number}
+ */ 
 
-// For example, given n = 2, return 1 (2 = 1 + 1); given n = 10, return 36 (10 = 3 + 3 + 4).
+// 7 => 3 + 4 = 12
+// 8 => 3 + 3 + 2 = 18
+// 9 => 3 + 3 + 3 = 27
+// 10 => 3 + 3 + 4 = 36
+// 11 = > 3 + 3 + 3 + 2 = 54
 
+var integerBreak = function(n) {
+    var result = 1;
+    
+    if (n === 2) {
+        return 1;
+    }
+    
+    if (n === 3) {
+        return 2;
+    }
+    
+    if (n === 4) {
+        return 4;
+    }
+    
+    while (n > 0) {
+        
+        if (n === 4) {
+            result *= 4;
+            n -= 4;
+        } else if ((n - 3) >= 0) {
+            result *= 3;
+            n -= 3;
+        } else if ((n - 2) >= 0){
+            result *=2;
+            n -= 2;
+        } else {
+            result *= 1;
+            n -= 1;
+        }
+    }
+    
+    return result;
+};
