089794eccd7e46bfb1a1c61d13e82d584f6e1b7f
David Hopper <davidhopper@Davids-MacBook-Pro.local>
2016-08-20 12:04:05 -0700

    Complete reverseWords to pass all tests.  Optimizations to come..

---
+++ reverseWords.js
@@ -0,0 +1,26 @@
+/**
+ * @param {string} str
+ * @returns {string}
+ */
+var reverseWords = function(str) {
+    var words = [];
+    var temp = "";
+    var i;
+    var len = str.length;
+    
+    for (i = 0; i < len; i += 1) {
+        if (str[i] !== " ") {
+            temp += str[i];
+        } else {
+            if (temp !== "") {
+                words.push(temp);
+            }
+            temp = "";
+        }
+    }
+    if (temp !== "") {
+        words.push(temp);
+    }
+    
+    return words.reverse().join(" ");
+};
